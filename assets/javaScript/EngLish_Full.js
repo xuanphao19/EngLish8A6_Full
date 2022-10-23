1499c2468b7781157776809a0057dac0c71c0528
Nguyễn Thanh Hòa <83102917+xuanphao19@users.noreply.github.com>
2022-10-23 12:46:15 +0700

    Add files via upload

--- assets/javaScript/EngLish_Full.js
+++ assets/javaScript/EngLish_Full.js
@@ -1,95 +1,3 @@
-// Vẽ Canvas:
-{
-  var canvas = document.getElementById("canvas");
-  var ctx = canvas.getContext("2d");
-  var radius = canvas.height / 2;
-  ctx.translate(radius, radius);
-  radius = radius * 0.95;
-  setInterval(drawClock, 1000);
-
-  function drawClock() {
-    drawFace(ctx, radius);
-    drawNumbers(ctx, radius);
-    drawTime(ctx, radius);
-  }
-
-  function drawFace(ctx, radius) {
-    var grad;
-    ctx.beginPath();
-    ctx.arc(0, 0, radius, 0, 2 * Math.PI);
-    // ctx.fillStyle = "violet";
-    ctx.fill();
-    var img = document.getElementById("scream");
-    ctx.drawImage(img, -132, -132, 265, 285);
-    grad = ctx.createRadialGradient(0, 0, radius * 0.85, 0, 0, radius * 1.18);
-    grad.addColorStop(0, "#88cd88");
-    grad.addColorStop(0.4, "#fff");
-    grad.addColorStop(1, "#eb94dd");
-    // grad.addColorStop(0, "#333");
-    // grad.addColorStop(0.4, "white");
-    // grad.addColorStop(1, "#333");
-    ctx.strokeStyle = grad;
-    ctx.lineWidth = radius * 0.14;
-    ctx.stroke();
-    ctx.beginPath();
-    ctx.arc(0, 0, radius * 0.075, 0, 2 * Math.PI);
-    ctx.fillStyle = "blue";
-    ctx.strokeStyle = "#0000ffb3";
-    ctx.fill();
-  }
-
-  function drawNumbers(ctx, radius) {
-    var ang;
-    var num;
-    var gradient;
-    ctx.font = radius * 0.16 + "px Verdana";
-    ctx.textBaseline = "middle";
-    ctx.textAlign = "center";
-    ctx.fillStyle = "rgb(239 0 255)";
-    for (num = 1; num < 13; num++) {
-      ang = (num * Math.PI) / 6;
-      ctx.rotate(ang);
-      ctx.translate(0, -radius * 0.8);
-      ctx.rotate(-ang);
-      ctx.fillText(num.toString(), 0, 0);
-      ctx.rotate(ang);
-      ctx.translate(0, radius * 0.8);
-      ctx.rotate(-ang);
-    }
-  }
-
-  function drawTime(ctx, radius) {
-    var now = new Date();
-    var hour = now.getHours();
-    var minute = now.getMinutes();
-    var second = now.getSeconds();
-    //hour
-    hour = hour % 12;
-    hour =
-      (hour * Math.PI) / 6 +
-      (minute * Math.PI) / (6 * 60) +
-      (second * Math.PI) / (360 * 60);
-    drawHand(ctx, hour, radius * 0.46, radius * 0.08);
-    //minute
-    minute = (minute * Math.PI) / 30 + (second * Math.PI) / (30 * 60);
-    drawHand(ctx, minute, radius * 0.7, radius * 0.065);
-    // second
-    second = (second * Math.PI) / 30;
-    drawHand(ctx, second, radius * 0.82, radius * 0.02);
-  }
-
-  function drawHand(ctx, pos, length, width) {
-    ctx.beginPath();
-    ctx.lineWidth = width;
-    ctx.lineCap = "round";
-    ctx.moveTo(0, 0);
-    ctx.rotate(pos);
-    ctx.lineTo(0, -length);
-    ctx.stroke();
-    ctx.rotate(-pos);
-  }
-}
-
 // ***************************************** Create App *********************************************
 var AppElement = document.querySelector("#App");
 var $ = AppElement.querySelector(".App_content");
@@ -470,7 +378,7 @@ function testValue(answerElement) {
     if (correctAnswer !== answerValue) {
       suggestionsMsg.textContent = "";
       if (!answerValue) {
-        return (warningMsgs = `Mỗi ⭐ bằng 1k NÈ. <br> Đang chờ bạn CHINH PHỤC hem!<br>*  *  *<br> "Không có Tri thức<br> là TỰ làm nhục CHÍNH MÌNH!" `);
+        return (warningMsgs = `Mỗi ⭐ bằng 1k NÈ. <br> ⭐ Đang chờ bạn CHINH PHỤC hem!<br>*  *  *<br> "Không có Tri thức<br> là TỰ làm nhục CHÍNH MÌNH!" `);
       } else if (!regex.test(answerValue)) {
         return `Không sử dụng khoảng trắng <br> để bắt đầu hoặc kết thúc`;
       } else if (!/[A-Z]/.test(answerValue)) {
@@ -517,6 +425,7 @@ clearErrorMsg = () => {
 var cardNext = $.querySelector("#next");
 var congratulationMusic = audioLists[9];
 var medals = "";
+var stars = $.querySelector(".star");
 cardNext.addEventListener("click", () => {
   i = i;
   console.log(`111111111111111`, i, `i + 1 =`, i + 1, questionId);
@@ -592,9 +501,8 @@ cardNext.addEventListener("click", () => {
       createRandomSong(songs);
     }
   }
-  console.log(medals);
+  stars.textContent = `${medals}`;
 });
-
 // Xóa massage lỗi và input value khi focus input:
 var flipCardInner = $.querySelector(".flip-card-inner");
 answerElement.addEventListener("focus", function handleClearError(e) {
